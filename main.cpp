2463369ec3756c6171caab48281afea277ed576b
sledge79 <tonywalsh1511@gmail.com>
2023-07-29 15:46:26 +0000

    First commit with loop
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+
 int main()
 {
-    while (true) {
+    printf("Lab 2 program \r\n");
+    int max_term = 10;
+    int term_1 = 0;
+    int term_2 = 1;
+    int next_term = 0;
+    for(int i=1; i<max_term; i++){
+
+        if(i == 1)
+           printf("%d, ", term_1);
+        if (i == 2)
+           printf("%d, ", term_2);
+        else{
+
+            next_term = term_1 + term_2;
+            term_1 = term_2;
+            term_2 = next_term;
+            printf("%d, ", next_term);
+        }      
+    }
+printf("\r\n");    
+while (true) {
 
     }
 }
